860cfce227ace8e1ef5ba9dcf6e6ea043c621b04
oawood93 <oa.wood@outlook.com>
2022-02-14 16:46:42 +0000

    refactor unit test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,15 +1,17 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+
     @Test
     void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
-        GildedRose app = new GildedRose(items);
+        Item item = new Item("foo", 0, 0);
+        GildedRose app = setUpGildedRose(item);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
@@ -17,8 +19,8 @@ class GildedRoseTest {
     @Test
     void
     should_not_decrease_sell_in_of_sulfuras_items(){
-        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 10, 10)};
-        GildedRose app = new GildedRose(items);
+        Item sulfurasItem = new Item("Sulfuras, Hand of Ragnaros", 10, 10);
+        GildedRose app = setUpGildedRose(sulfurasItem);
         app.updateQuality();
         assertEquals(10, app.items[0].sellIn);
     }
@@ -26,10 +28,23 @@ class GildedRoseTest {
     @Test
     void
     should_not_decrease_quality_of_sulfuras_items(){
-        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 10, 10)};
-        GildedRose app = new GildedRose(items);
+        Item sulfurasItem = new Item("Sulfuras, Hand of Ragnaros", 10, 10);
+        GildedRose app = setUpGildedRose(sulfurasItem);
         app.updateQuality();
         assertEquals(10, app.items[0].quality);
+    }
+
+    @Test
+    void
+    Should_increase_quality_of_aged_brie_if_less_than_50(){
+
+    }
+
+    private GildedRose setUpGildedRose(Item item){
+        return setUpGildedRose(new Item[]{item});
+    }
+    private GildedRose setUpGildedRose(Item[] items){
+        return new GildedRose(items);
     }
 
 }
