e50615143469bb73afaeca4c77a15b3888fdc2c9
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-15 10:05:09 +0100

    refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,23 +13,19 @@ class GildedRose {
         for (Item item : items) {
             if (Objects.equals(item.name, "Aged Brie")
                 || Objects.equals(item.name, "Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality < 50) {
-                        increaseQualityFor(item);
 
-                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.sellIn < 11) {
-                                if (item.quality < 50) {
-                                    increaseQualityFor(item);
-                                }
-                            }
+                    increaseQualityFor(item);
 
-                            if (item.sellIn < 6) {
-                                if (item.quality < 50) {
-                                    increaseQualityFor(item);
-                                }
-                            }
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.sellIn < 11) {
+                            increaseQualityFor(item);
+                        }
+
+                        if (item.sellIn < 6) {
+                            increaseQualityFor(item);
                         }
                     }
+
                 } else {
                 if (item.quality > 0) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -44,9 +40,9 @@ class GildedRose {
 
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) {
+
                         increaseQualityFor(item);
-                    }
+
                 } else {
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = 0;
@@ -63,7 +59,9 @@ class GildedRose {
     }
 
     private void increaseQualityFor(Item item) {
-        item.quality = item.quality + 1;
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
     }
 
     private void decreaseQualityFor(Item item) {
