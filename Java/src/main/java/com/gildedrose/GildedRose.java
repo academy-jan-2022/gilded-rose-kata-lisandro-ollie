15c8962ed306407a6a74602fcd8e931d0515b099
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-15 10:07:12 +0100

    refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -27,11 +27,11 @@ class GildedRose {
                     }
 
                 } else {
-                if (item.quality > 0) {
+
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                         decreaseQualityFor(item);
                     }
-                }
+
             }
 
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -47,10 +47,10 @@ class GildedRose {
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = 0;
                     } else {
-                        if (item.quality > 0) {
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+
                                 decreaseQualityFor(item);
-                            }
+
                         }
                     }
                 }
@@ -65,7 +65,9 @@ class GildedRose {
     }
 
     private void decreaseQualityFor(Item item) {
-        item.quality = item.quality - 1;
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
     }
 
     private void decreaseSellInFor(Item item) {
